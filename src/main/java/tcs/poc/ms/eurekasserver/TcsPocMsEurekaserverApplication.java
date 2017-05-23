2f9c4e34f5d7e9c3c665e3c2f9501d99fd6e1629
tcs-ms-poc <sreeraj.ar@outlook.com>
2017-05-23 16:43:31 -0400

    code base to start

---
+++ src/main/java/tcs/poc/ms/eurekasserver/TcsPocMsEurekaserverApplication.java
@@ -0,0 +1,12 @@
+package tcs.poc.ms.eurekasserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcsPocMsEurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsPocMsEurekaserverApplication.class, args);
+	}
+}
---
+++ src/test/java/tcs/poc/ms/eurekasserver/TcsPocMsEurekaserverApplicationTests.java
@@ -0,0 +1,16 @@
+package tcs.poc.ms.eurekasserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TcsPocMsEurekaserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
