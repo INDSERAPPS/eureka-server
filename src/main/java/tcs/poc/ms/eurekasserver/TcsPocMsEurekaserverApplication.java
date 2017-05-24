2844116374001ca6ed8b1db9f03763cb88631dc1
tcs-ms-poc <sreeraj.ar@outlook.com>
2017-05-23 23:12:57 -0400

    added the configuration - application.yml and updated the pom file. Also added annotation for eureka server and client.

--- src/main/java/tcs/poc/ms/eurekasserver/TcsPocMsEurekaserverApplication.java
+++ src/main/java/tcs/poc/ms/eurekasserver/TcsPocMsEurekaserverApplication.java
@@ -2,8 +2,12 @@ package tcs.poc.ms.eurekasserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
 public class TcsPocMsEurekaserverApplication {
 
 	public static void main(String[] args) {
